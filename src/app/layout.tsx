eeba06e591ef5abcaa189f2924de7484984533c7
Eduardo Seiti <eduseiti@hotmail.com>
2025-03-14 16:11:05 +0900

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Piano Virbrato",
+  description: "Created by Eduardo Seiti Yamatsumi",
+};
 export default function RootLayout({
   children,
 }: {
